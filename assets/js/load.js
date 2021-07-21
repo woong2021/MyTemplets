01174ac7f870f19396a3769dbcf2dcf3cc2389d8
woong2021 <meimi101@naver.com>
2021-07-21 14:29:17 +0900

    ### 2021.07.21
    ```
    - [LNB] 하위 메뉴가 아래로 펼쳐지는 LNB 추가
    - [LNB] 하위 메뉴가 옆으로 평쳐지는 LNB 마크업 작업
    ```

--- assets/js/load.js
+++ assets/js/load.js
@@ -8,6 +8,7 @@ $(function(){
     //LNB
     $("#lnbThema1").load("../lnb/thema1.html");
     $("#lnbThema2").load("../lnb/thema2.html");
+    $("#lnbThema3").load("../lnb/thema3.html");
 
     //footer
     $("#footer").load("../layout/footer.html");
