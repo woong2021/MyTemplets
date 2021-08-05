37dd8e2da7d2ecf642aa4be846e69224ccd2a42c
woong2021 <meimi101@naver.com>
2021-08-05 18:10:44 +0900

    ### 2021.08.05
    ```
    - [Mobile] "LNB"메뉴 애니메이션 추가 및 스크립트 작업
    - [Templet] 반응형 프로토 작업진행
    ```

--- assets/js/common.js
+++ assets/js/common.js
@@ -73,4 +73,53 @@ $(function(){
             $(this).children('.sub-menu-example').stop().slideUp();
         }
     })
-})
+
+    //response nav
+    var select_attr = $('header#responsive nav a, header#responsive nav button');
+
+    $(window).on('resize load', function(){
+        var width = $(window).width();
+
+        if (width < 721){
+            attrs();
+        }else{
+            removeAttrs();
+            $('.dim').fadeOut();
+            $('header#responsive nav').removeClass('on');
+        }
+    });
+
+    $('.btn_nav').on({
+        "click" : function(){
+            var width = $(window).width();
+            if(width < 721){
+                $('header#responsive nav').addClass('on');
+                $('header#responsive nav a:first').focus();
+                $('.dim').fadeIn();
+
+                removeAttrs();
+            }
+        }
+    });
+
+    $('header#responsive .close').on({
+        "click" : function(){
+            $(this).parent().removeClass('on');
+            $('.dim').fadeOut();
+            
+            attrs();
+        }
+    });
+
+    function attrs(){
+        select_attr.attr({
+            'aria-hidden' : 'true',
+            'tabindex' : '-1'
+        });
+    }
+
+    function removeAttrs(){
+        select_attr.removeAttr('aria-hidden').removeAttr('tabindex');
+    }
+
+});
